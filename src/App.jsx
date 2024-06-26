e148cc45b55e6235266f4a6a0c769199c6cf9b65
CRXKXWYI49 <134039857+CRXKXWYI49@users.noreply.github.com>
2024-06-26 12:48:57 -1000

    Modify browser router layout

--- src/App.jsx
+++ src/App.jsx
@@ -16,28 +16,28 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 function App() {
   return (
     <>
-      <section className="">
-        <BackgroundVideo/>
-        <div className="w-full flex justify-center">
-          <div className="flex flex-col items-center gap-10 w-[80%] lg:w-[1024px]">
-            <BrowserRouter>
-              <Navbar className="w-[80%] lg:w-[1024px]"/>
-              <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/people" element={<People/>}/>
-                <Route path="/publications" element={<Publications/>}/>
-                <Route path="/research" element={<Research/>}/>
-                <Route path="/outreach" element={<Outreach/>}/>
-                <Route path="/laboratories" element={<Laboratories/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-                <Route path="/joinus" element={<JoinUs/>}/>
-                <Route path="/Disclaimer" element={<Disclaimer/>}/>
-                <Route path="/shirash" element={<___/>}/>
-              </Routes>   
-            </BrowserRouter>         
-          </div>
-        </div>
-        <Footer/>
+      <section>
+        <BrowserRouter>
+          <BackgroundVideo/>
+            <Navbar className="w-[80%] lg:w-[1024px]"/>
+            <div className="w-full flex justify-center">
+              <div className="flex flex-col items-center gap-10 w-[80%] lg:w-[1024px]">
+                  <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/people" element={<People/>}/>
+                    <Route path="/publications" element={<Publications/>}/>
+                    <Route path="/research" element={<Research/>}/>
+                    <Route path="/outreach" element={<Outreach/>}/>
+                    <Route path="/laboratories" element={<Laboratories/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                    <Route path="/joinus" element={<JoinUs/>}/>
+                    <Route path="/Disclaimer" element={<Disclaimer/>}/>
+                    <Route path="/shirash" element={<___/>}/>
+                  </Routes>   
+              </div>
+            </div>
+          <Footer/>
+        </BrowserRouter>         
       </section>
     </>
   )
