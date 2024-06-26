5a29412bbf6421118ba3cd8538dcad9f8c1e73a0
CRXKXWYI49 <134039857+CRXKXWYI49@users.noreply.github.com>
2024-06-26 13:05:49 -1000

    Update App.js again

--- src/App.jsx
+++ src/App.jsx
@@ -17,29 +17,27 @@ function App() {
   return (
     <>
       <BrowserRouter>
-          <section>
-            <BackgroundVideo/>
-              <div className="w-full flex justify-center">
-                <div className="flex flex-col items-center gap-10 w-[80%] lg:w-[1024px]">
-                  <Navbar className="w-[80%] lg:w-[1024px]"/>
-                    <Routes>
-                      <Route path="/" element={<Home/>}/>
-                      <Route path="/people" element={<People/>}/>
-                      <Route path="/publications" element={<Publications/>}/>
-                      <Route path="/research" element={<Research/>}/>
-                      <Route path="/outreach" element={<Outreach/>}/>
-                      <Route path="/laboratories" element={<Laboratories/>}/>
-                      <Route path="/contact" element={<Contact/>}/>
-                      <Route path="/joinus" element={<JoinUs/>}/>
-                      <Route path="/Disclaimer" element={<Disclaimer/>}/>
-                      <Route path="/shirash" element={<___/>}/>
-                    </Routes>   
-                  </div>
-                </div>
-              <Footer/>
-            </section>
-        </BrowserRouter>         
-      </>
+        <BackgroundVideo/>
+        <div className="w-full flex justify-center">
+          <div className="flex flex-col items-center gap-10 w-[80%] lg:w-[1024px]">
+            <Navbar className="w-[80%] lg:w-[1024px]"/>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/people" element={<People/>}/>
+              <Route path="/publications" element={<Publications/>}/>
+              <Route path="/research" element={<Research/>}/>
+              <Route path="/outreach" element={<Outreach/>}/>
+              <Route path="/laboratories" element={<Laboratories/>}/>
+              <Route path="/contact" element={<Contact/>}/>
+              <Route path="/joinus" element={<JoinUs/>}/>
+              <Route path="/Disclaimer" element={<Disclaimer/>}/>
+              <Route path="/shirash" element={<___/>}/>
+            </Routes>   
+          </div>
+        </div>
+        <Footer/>
+      </BrowserRouter>         
+    </>
   )
 }
 
