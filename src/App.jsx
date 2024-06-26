948a324ac94de553b8f171b6df3da2901c1aa869
CRXKXWYI49 <134039857+CRXKXWYI49@users.noreply.github.com>
2024-06-26 13:14:59 -1000

    Replace browser router with hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
-import { Home,
-         People,
-         Publications,
-         Research,
-         Outreach,
-         Laboratories,
-         JoinUs,
-         Contact,
-         Disclaimer,
-         ___ } from "./pages";
-import { Navbar,
-         BackgroundVideo,
-         Footer } from "./components"
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import {
+  Home,
+  People,
+  Publications,
+  Research,
+  Outreach,
+  Laboratories,
+  JoinUs,
+  Contact,
+  Disclaimer,
+  ___
+} from "./pages";
+import { Navbar, BackgroundVideo, Footer } from "./components";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <BackgroundVideo/>
+      <HashRouter>
+        <BackgroundVideo />
         <div className="w-full flex justify-center">
           <div className="flex flex-col items-center gap-10 w-[80%] lg:w-[1024px]">
-            <Navbar className="w-[80%] lg:w-[1024px]"/>
+            <Navbar className="w-[80%] lg:w-[1024px]" />
             <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/people" element={<People/>}/>
-              <Route path="/publications" element={<Publications/>}/>
-              <Route path="/research" element={<Research/>}/>
-              <Route path="/outreach" element={<Outreach/>}/>
-              <Route path="/laboratories" element={<Laboratories/>}/>
-              <Route path="/contact" element={<Contact/>}/>
-              <Route path="/joinus" element={<JoinUs/>}/>
-              <Route path="/Disclaimer" element={<Disclaimer/>}/>
-              <Route path="/shirash" element={<___/>}/>
-            </Routes>   
+              <Route path="/" element={<Home />} />
+              <Route path="/people" element={<People />} />
+              <Route path="/publications" element={<Publications />} />
+              <Route path="/research" element={<Research />} />
+              <Route path="/outreach" element={<Outreach />} />
+              <Route path="/laboratories" element={<Laboratories />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/joinus" element={<JoinUs />} />
+              <Route path="/disclaimer" element={<Disclaimer />} />
+              <Route path="/shirash" element={<___ />} />
+            </Routes>
           </div>
         </div>
-        <Footer/>
-      </BrowserRouter>         
+        <Footer />
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
